adc003354111b5dbdb3d7a1b1b1379a78466ecb7
Nardianian <145938851+Nardianian@users.noreply.github.com>
2026-04-04 18:06:42 +0200

    Refactor Euclidean and HyperEuclidean class comments

--- Source/EuclideanSet.h
+++ Source/EuclideanSet.h
@@ -5,24 +5,22 @@
 #include <string>
 #include <algorithm>
 
-// Base class for the computation of classical Euclidean sequences
 class Euclidean {
 public:
     Euclidean(int pulses = 1, int steps = 1);
     virtual ~Euclidean() {}
 
-    // Function to calculate a Euclidean sequence
     virtual std::vector<int> generateSequence();
 
 protected:
-    int pulses;  // Number of pulses
-    int steps;   // Number of total steps
-    std::vector<int> sequence; // Resulting sequence
+    int pulses;  // Pulses Number
+    int steps;   // Steps Number
+    std::vector<int> sequence; // Sequence
 
-    void computeClassicEuclidean(); // Classical Euclidean algorithm
+    void computeClassicEuclidean(); // Euclidean classic
 };
 
-// Derived class for Euclidean Hypereuclidean (HyperEuclidean)
+// HyperEuclidean
 class HyperEuclidean : public Euclidean {
 public:
     HyperEuclidean(int pulses = 1, int steps = 1, int depth = 1);
@@ -38,5 +36,4 @@ private:
     std::vector<int> selectByIndex(const std::vector<int>& source, const std::vector<int>& indices);
     std::vector<int> computeIOI(const std::vector<int>& set);
 };
-
 
